fb8ea5c5069c139a39963e88599ed903fd092009
XamuelUG <samweko@gmail.com>
2021-09-25 10:39:11 +0300

    client started

---
+++ client/src/app.js
@@ -0,0 +1,16 @@
+import React, { Component } from "react";
+
+//the styling/css
+import "./app.css";
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+  render() {
+    return <div className="app-ctr"></div>;
+  }
+}
+
+export default App;
